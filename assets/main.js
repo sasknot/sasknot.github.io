5fac7e69cd5f3c17c7cbd79b0bae5332d6325aa9
Rafael F. Silva <rafaelfsilva1@gmail.com>
2021-03-30 16:43:14 -0300

    add project

--- assets/main.js
+++ assets/main.js
@@ -1,55 +1,63 @@
 var LOCALE = {
   'name': {
-    'pt-BR': 'Rafael Francisco Shiroma da Silva',
-    'ja-JP': '城間ダシルバ ラファエル'
+    'pt': 'Rafael Francisco Shiroma da Silva',
+    'ja': '城間ダシルバ ラファエル'
   },
   'position': {
-    'pt-BR': 'Desenvolvedor de Software',
-    'ja-JP': 'ソフトウェア開発者'
+    'pt': 'Desenvolvedor de Software',
+    'ja': 'ソフトウェア開発者'
   },
   'stack': {
-    'pt-BR': 'stack atual é',
-    'ja-JP': 'スタックは'
+    'pt': 'stack atual é',
+    'ja': 'スタックは'
   },
   'projects': {
-    'pt-BR': 'projetos',
-    'ja-JP': 'プロジェクト'
+    'pt': 'projetos',
+    'ja': 'プロジェクト'
   },
   'projects-repo': {
-    'pt-BR': 'Repositório Git',
-    'ja-JP': 'gitリポジトリ'
+    'pt': 'Repositório Git',
+    'ja': 'gitリポジトリ'
   },
   'projects-demo': {
-    'pt-BR': 'Demonstração',
-    'ja-JP': 'ライブデモ'
+    'pt': 'Demonstração',
+    'ja': 'ライブデモ'
+  },
+  'projects-agenda': {
+    'pt': 'aprendendo conceitos e implementações de Vue3 + Vite + Tailwind',
+    'ja': 'Vue3 + Vite + Tailwindのコンセプトや実現を勉強しています'
+  },
+  'projects-nlw': {
+    'pt': 'Aprendendo ReactJS e React Native com a',
+    'ja': 'ReactJSとReact Nativeの勉強しています。'
   },
   'projects-moveit': {
-    'pt-BR': 'complete desafios de exercícios físicos e se motive com suas vitórias.',
-    'ja-JP': '運動の挑戦を完了して優勝すると気合を入れます。'
+    'pt': 'complete desafios de exercícios físicos e se motive com suas vitórias.',
+    'ja': '運動の挑戦を完了して優勝すると気合を入れます。'
   },
   'projects-happy': {
-    'pt-BR': '"Happy" é um meio para conectar orfanatos e quem os quer visitar. Está separada em 3 apss: api, web e mobile.',
-    'ja-JP': '「ハッピー」は、見に行きたい方と孤児院をつなぐ方法です。 3つのアプリに分かれています： api、web、モバイル。'
+    'pt': '"Happy" é um meio para conectar orfanatos e quem os quer visitar. Está separada em 3 apss: api, web e mobile.',
+    'ja': '「ハッピー」は、見に行きたい方と孤児院をつなぐ方法です。 3つのアプリに分かれています： api、web、モバイル。'
   },
   'projects-proffy': {
-    'pt-BR': '"Proffy" é um meio para conectar professores a alunos. Está separada em 3 apps: api, web e mobile.',
-    'ja-JP': '「プロフィー」は、教師と生徒をつなぐ方法です。 3つのアプリに分かれています： api、web、モバイル。'
+    'pt': '"Proffy" é um meio para conectar professores a alunos. Está separada em 3 apps: api, web e mobile.',
+    'ja': '「プロフィー」は、教師と生徒をつなぐ方法です。 3つのアプリに分かれています： api、web、モバイル。'
   },
   'projects-sg': {
-    'pt-BR': 'uma rede social para gamers (em desenvolvimento)',
-    'ja-JP': 'ゲーマーへのソーシャルネットワーク（制作中）。'
+    'pt': 'uma rede social para gamers (em desenvolvimento)',
+    'ja': 'ゲーマーへのソーシャルネットワーク（制作中）。'
   },
   'projects-gc': {
-    'pt-BR': 'um plugin jQuery muito antigo para colocar elementos em um grid',
-    'ja-JP': '要素をグリッドに配置するためのとても古いjQueryプラグイン。'
+    'pt': 'um plugin jQuery muito antigo para colocar elementos em um grid',
+    'ja': '要素をグリッドに配置するためのとても古いjQueryプラグイン。'
   },
   'thanks': {
-    'pt-BR': 'obrigado pela leitura!',
-    'ja-JP': '読んでくれてありがとうございました！'
+    'pt': 'obrigado pela leitura!',
+    'ja': '読んでくれてありがとうございました！'
   }
 };
 
-const LANGUAGE_DEFAULT = 'en-US';
+const LANGUAGE_DEFAULT = 'en';
 const $refs = {};
 
 Object.keys(LOCALE).forEach(function (refId) {
